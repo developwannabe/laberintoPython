8e3861400e2f04a2cbc50c8db77e62dc36934097
Illan <illan.martinez@hotmail.com>
2023-06-06 19:23:36 +0200

    Ampliado playground para todas las funciones

--- Playground.py
+++ Playground.py
@@ -33,12 +33,12 @@ if opcion == "2":
     while not juego.fase.esFinal():
         if forma == "Cuadrado":
             print("¿Qué deseas hacer?\n    A. Atacar\n    1. Mover al norte\n    2. Mover al este\n    3. Mover al oeste\n    4. Mover al sur\n",
-                  "   5. Abrir Puertas\n    6. Lanzar bichos\n    H. Obtener hijos de la posición del personaje\n",
-                  "   C. Obtener Comandos\n    I. Mostrar inventario")
+                  "   5. Abrir Puertas\n    6. Lanzar bichos\n    7. Mostrar comandos bolsa\n    8. Mostrar comandos cuerpo\n",   
+                  "   H. Obtener hijos de la posición del personaje\n    C. Obtener Comandos\n    I. Mostrar inventario")
         if forma == "Rombo":
             print("¿Qué deseas hacer?\n    A. Atacar\n    1. Mover al noreste\n    2. Mover al noroeste\n    3. Mover al sureste\n    4. Mover al suroeste\n",
-              "   5. Abrir Puertas\n    6. Lanzar bichos\n    H. Obtener hijos de la posición del personaje\n",
-              "   C. Obtener Comandos\n    I. Mostrar inventario")
+              "   5. Abrir Puertas\n    6. Lanzar bichos\n    7. Mostrar comandos bolsa\n    8. Mostrar comandos cuerpo\n",
+              "   H. Obtener hijos de la posición del personaje\n    C. Obtener Comandos\n    I. Mostrar inventario")
         eleccion=input()
         if forma == "Cuadrado":
             if eleccion == "1":
@@ -62,8 +62,47 @@ if opcion == "2":
             juego.abrirPuertas()
         if eleccion == "6":
             juego.lanzarBichos()
+        if eleccion == "7":
+            i = 0
+            coms=personaje.bolsa.obtenerComandos(personaje)
+            if len(coms) > 0:
+                for com in coms:
+                    print("    ",i,". ",com,"\n")
+                    i += 1
+                el = input()
+                el = int(el)
+                coms[el].ejecutar(personaje)
+            else:
+                print("No hay objetos en la bolsa.")
+        if eleccion == "8":
+            print("    1. Mano derecha\n    2. Mano izquierda")
+            el = input()
+            el = int(el)
+            if el == 1:
+                i = 0
+                if personaje.cuerpo.mDerecha is not None:
+                    for com in (coms:=personaje.cuerpo.mDerecha.comandos):
+                        print("    ",i,". ",com,"\n")
+                        i += 1
+                    ele = input()
+                    ele = int(ele)
+                    coms[ele].ejecutar(personaje)
+                else:
+                    print("No hay nada en la mano derecha.")
+            if el == 2:
+                i = 0
+                if personaje.cuerpo.mIzquierda is not None:
+                    for com in (coms:=personaje.cuerpo.mIzquierda.comandos):
+                        print("    ",i,". ",com,"\n")
+                        i += 1
+                    ele = input()
+                    ele = int(ele)
+                    coms[ele].ejecutar(personaje)
+                else:
+                    print("No hay nada en la mano izquierda.")
+
         if eleccion == "a" or eleccion == "A":
-            personaje.atacar()#TODO:Añadir inventario y cuerpo.
+            personaje.atacar()
 
         if eleccion == "i" or eleccion == "I":
             i = 0
